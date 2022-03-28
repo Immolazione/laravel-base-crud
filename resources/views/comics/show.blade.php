b76fbadaaad5dd1f19656f3978fbd2197e6bd7e1
Immolazione <federico.deered@gmail.com>
2022-03-28 06:18:38 +0200

    minor changes

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -16,7 +16,7 @@
         <li>{{ $comic['sale_date'] }}</li>
         <li>{{ $comic['type'] }}</li>
          <a href="{{ {{--URL::previous()--}} route('comics.index') }}">Indietro</a> 
-         <a href="{{route('comics.edit') }}">Modifica</a> 
+         <a href="{{route('comics.edit', $comic->id) }}">Modifica</a> 
     </ul>
 </body>
 </html>
